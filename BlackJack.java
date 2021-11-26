dd7795bc81156ffed1ec92d69a595bbbdf5dfee4
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2021-11-26 22:11:29 +0200

    added endgame

--- BlackJack.java
+++ BlackJack.java
@@ -36,23 +36,50 @@ public class BlackJack {
 
         // player 3 is the dealer
         if (!player[3].isBust()) {
-            while (true) {
-                if (scanner.next().equals("hit")) {
-                    game.players[i].addCard(game.drawCard());
-                    if (game.players[i].isBust()) {
-                        System.out.println("Bust!");
-                        break;
-                    } else if (game.players[i].isBlackjack()) {
-                        System.out.println("Blackjack!");
-                        break;
-                    }
-                } else if (scanner.next().equals("stand")) {
-                    game.stand();
-                    break;
-                } else {
-                    System.out.println("Please enter 'hit' or 'stand'");
-                }
+            while (!player[3].isBust() && !player[3].isBlackjack()
+                    && max(players[0].getScore(),
+                            max(players[1].getScore(), players[2].getScore())) <= players[3].getScore()) {
+
+                players[3].addCard(game.drawCard());
             }
         }
+
+        // Decides whether the game is a tie or not.
+        int numBlackJack = 0;
+        for (int i = 0; i < 4; i++) {
+            if (players[i].isBlackjack()) {
+                numBlackJack++;
+            }
+        }
+        if (numBlackJack == 1) {
+            for (int i = 0; i < 4; i++) {
+                if (players[i].isBlackjack()) {
+                    System.out.println("Player " + toString(i + 1) + " wins!");
+                }
+            }
+        } else if (numBlackJack > 1) {
+            System.out.println("PUSH!");
+        } else {
+            if (players[0].getScore() > players[1].getScore()
+                    && players[0].getScore() > players[2].getScore()
+                    && players[0].getScore() > players[3].getScore()) {
+                System.out.println("Player 1 wins!");
+            } else if (players[1].getScore() > players[0].getScore()
+                    && players[1].getScore() > players[2].getScore()
+                    && players[1].getScore() > players[3].getScore()) {
+                System.out.println("Player 2 wins!");
+            } else if (players[2].getScore() > players[0].getScore()
+                    && players[2].getScore() > players[1].getScore()
+                    && players[2].getScore() > players[3].getScore()) {
+                System.out.println("Player 3 wins!");
+            } else if (players[3].getScore() > players[0].getScore()
+                    && players[3].getScore() > players[1].getScore()
+                    && players[3].getScore() > players[2].getScore()) {
+                System.out.println("Player 4 wins!");
+            } else {
+                System.out.println("PUSH!");
+            }
+        }
+
     }
 }
